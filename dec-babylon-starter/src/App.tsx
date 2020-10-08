f09fb7f5fc8534e50c2e5015622cf087a788dd80
Eric Greene <eric@t4d.io>
2020-10-08 14:13:13 -0400

    add solution to exercise 2

--- dec-babylon-starter/src/App.tsx
+++ dec-babylon-starter/src/App.tsx
@@ -4,9 +4,28 @@ import { Engine } from 'react-babylonjs';
 
 import { TheScene } from './components/TheScene';
 
+const toggleLight = (lights: boolean[], index: number) => {
+  const newLights = [...lights];
+  newLights[index] = !newLights[index];
+  return newLights;
+};
+
+const updateLightIntensity = (
+  lightIntensity: number[],
+  index: number,
+  value: number,
+) => {
+  const newLightIntensity = [...lightIntensity];
+  newLightIntensity[index] = value;
+  return newLightIntensity;
+};
+
 export function App() {
   const [activeCamera, setActiveCamera] = useState('arcrotate');
   const [arcRotateRadius, setArcRotateRadius] = useState(10);
+
+  const [enableLights, setEnableLights] = useState([true, true, true, true]);
+  const [lightIntensity, setLightIntensity] = useState([10, 10, 10, 10]);
 
   return (
     <>
@@ -32,11 +51,81 @@ export function App() {
             </>
           )}
         </fieldset>
+        <fieldset>
+          <button
+            type="button"
+            onClick={() => setEnableLights(toggleLight(enableLights, 0))}>
+            Spotlight 0
+          </button>
+          <button
+            type="button"
+            onClick={() => setEnableLights(toggleLight(enableLights, 1))}>
+            Spotlight 1
+          </button>
+          <button
+            type="button"
+            onClick={() => setEnableLights(toggleLight(enableLights, 2))}>
+            Spotlight 2
+          </button>
+          <button
+            type="button"
+            onClick={() => setEnableLights(toggleLight(enableLights, 3))}>
+            Spotlight 3
+          </button>
+        </fieldset>
+        <fieldset>
+          <input
+            type="range"
+            value={lightIntensity[0]}
+            onChange={(e) =>
+              setLightIntensity(
+                updateLightIntensity(lightIntensity, 0, Number(e.target.value)),
+              )
+            }
+            min={0}
+            max={30}
+          />
+          <input
+            type="range"
+            value={lightIntensity[1]}
+            onChange={(e) =>
+              setLightIntensity(
+                updateLightIntensity(lightIntensity, 1, Number(e.target.value)),
+              )
+            }
+            min={0}
+            max={30}
+          />
+          <input
+            type="range"
+            value={lightIntensity[2]}
+            onChange={(e) =>
+              setLightIntensity(
+                updateLightIntensity(lightIntensity, 2, Number(e.target.value)),
+              )
+            }
+            min={0}
+            max={30}
+          />
+          <input
+            type="range"
+            value={lightIntensity[3]}
+            onChange={(e) =>
+              setLightIntensity(
+                updateLightIntensity(lightIntensity, 3, Number(e.target.value)),
+              )
+            }
+            min={0}
+            max={30}
+          />
+        </fieldset>
       </form>
       <Engine canvasId="renderCanvas" antialias>
         <TheScene
           activeCamera={activeCamera}
           arcRotateRadius={arcRotateRadius}
+          enableLights={enableLights}
+          lightIntensity={lightIntensity}
         />
       </Engine>
     </>
--- dec-babylon-starter/src/components/TheScene.tsx
+++ dec-babylon-starter/src/components/TheScene.tsx
@@ -1,15 +1,18 @@
 import React, { useRef, useEffect } from 'react';
-import { Scene } from 'react-babylonjs';
+import { Scene, CreatedInstance } from 'react-babylonjs';
 import {
   Vector3,
   ArcRotateCamera,
   UniversalCamera,
   Color3,
+  SpotLight,
 } from '@babylonjs/core';
 
 export type TheSceneProps = {
   activeCamera?: string;
   arcRotateRadius?: number;
+  enableLights: boolean[];
+  lightIntensity: number[];
 };
 
 // const cameraAdded = (...params: any[]) => {
@@ -22,7 +25,20 @@ export type TheSceneProps = {
 // onCameraRemovedObservable = { cameraRemoved };
 
 export function TheScene(props: TheSceneProps) {
-  const cameraRef = useRef({} as any);
+  const spotlightRef = useRef<CreatedInstance<SpotLight> | null>(null);
+
+  useEffect(() => {
+    if (spotlightRef.current && spotlightRef.current.hostInstance) {
+      console.dir(spotlightRef);
+
+      const spotlight = spotlightRef.current.hostInstance;
+
+      spotlight.setEnabled(props.enableLights[0]);
+      spotlight.intensity = props.lightIntensity[0] / 10;
+    }
+  }, [props.enableLights, props.lightIntensity]);
+
+  // const cameraRef = useRef({} as any);
 
   // useEffect(() => {
   //   if (cameraRef.current) {
@@ -38,7 +54,6 @@ export function TheScene(props: TheSceneProps) {
     <Scene>
       {props.activeCamera === 'arcrotate' && (
         <arcRotateCamera
-          ref={cameraRef}
           name="camera"
           alpha={Math.PI / 2}
           beta={0}
@@ -52,12 +67,14 @@ export function TheScene(props: TheSceneProps) {
       {/* <hemisphericLight name="light1" direction={new Vector3(0, 1, 0)} /> */}
       {/* <pointLight name="light2" position={new Vector3(0, 1, 3)} /> */}
       <spotLight
+        ref={spotlightRef}
         name="light1"
         position={new Vector3(0, 5, 5)}
         direction={new Vector3(0, -5, -10)}
         angle={Math.PI}
         exponent={20}
         diffuse={new Color3(1, 0, 0)}
+        intensity={1}
       />
       <spotLight
         name="light2"
@@ -66,23 +83,31 @@ export function TheScene(props: TheSceneProps) {
         angle={Math.PI}
         exponent={20}
         diffuse={new Color3(0, 1, 0)}
+        setEnabled={props.enableLights[1]}
+        intensity={props.lightIntensity[1] / 10}
       />
-      <spotLight
-        name="light3"
-        position={new Vector3(-5, 5, 0)}
-        direction={new Vector3(10, -5, 0)}
-        angle={Math.PI}
-        exponent={20}
-        diffuse={new Color3(0, 0, 1)}
-      />
-      <spotLight
-        name="light4"
-        position={new Vector3(5, 5, 0)}
-        direction={new Vector3(-10, -5, 0)}
-        angle={Math.PI}
-        exponent={20}
-        diffuse={new Color3(0, 0, 0)}
-      />
+      {props.enableLights[2] && (
+        <spotLight
+          name="light3"
+          position={new Vector3(-5, 5, 0)}
+          direction={new Vector3(10, -5, 0)}
+          angle={Math.PI}
+          exponent={20}
+          diffuse={new Color3(0, 0, 1)}
+          intensity={props.lightIntensity[2] / 10}
+        />
+      )}
+      {props.enableLights[3] && (
+        <spotLight
+          name="light4"
+          position={new Vector3(5, 5, 0)}
+          direction={new Vector3(-10, -5, 0)}
+          angle={Math.PI}
+          exponent={20}
+          diffuse={new Color3(1, 0, 1)}
+          intensity={props.lightIntensity[3] / 10}
+        />
+      )}
       <sphere name="sphere" diameter={2} position={new Vector3(4, 1, 4)} />
       <sphere name="sphere" diameter={2} position={new Vector3(4, 1, -4)} />
       <sphere name="sphere" diameter={2} position={new Vector3(-4, 1, -4)} />
