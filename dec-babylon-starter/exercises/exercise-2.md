d922afe40b301cd8f74c267a8f15c66939265d91
Eric Greene <eric@t4d.io>
2020-10-08 13:24:15 -0400

    add exercise 2

---
+++ dec-babylon-starter/exercises/exercise-2.md
@@ -0,0 +1,9 @@
+# Exercise 2
+
+1. Using the spheres and spotlights from class declarative demo and the DOM form, add buttons to the DOM form to toggle each spotlight.
+
+2. Use a range input for each spotlight to adjust intensity. The min value is 0 and the max value is 3, the increment should change in tenths.
+
+3. Implement at least one spotlight using a fully declarative approach, and one spotlight using an imperative approach within the overall declarative React code.
+
+4. Ensure it works!
--- dec-babylon-starter/src/components/TheScene.tsx
+++ dec-babylon-starter/src/components/TheScene.tsx
@@ -1,6 +1,11 @@
 import React, { useRef, useEffect } from 'react';
 import { Scene } from 'react-babylonjs';
-import { Vector3, ArcRotateCamera, UniversalCamera } from '@babylonjs/core';
+import {
+  Vector3,
+  ArcRotateCamera,
+  UniversalCamera,
+  Color3,
+} from '@babylonjs/core';
 
 export type TheSceneProps = {
   activeCamera?: string;
@@ -19,15 +24,15 @@ export type TheSceneProps = {
 export function TheScene(props: TheSceneProps) {
   const cameraRef = useRef({} as any);
 
-  useEffect(() => {
-    if (cameraRef.current) {
-      console.log(cameraRef.current.hostInstance);
+  // useEffect(() => {
+  //   if (cameraRef.current) {
+  //     console.log(cameraRef.current.hostInstance);
 
-      console.log(cameraRef.current.hostInstance.radius);
-      // very bad to do, the value assigned must come from some kind of state
-      cameraRef.current.hostInstance.radius = 20;
-    }
-  }, []);
+  //     console.log(cameraRef.current.hostInstance.radius);
+  //     // very bad to do, the value assigned must come from some kind of state
+  //     cameraRef.current.hostInstance.radius = 20;
+  //   }
+  // }, []);
 
   return (
     <Scene>
@@ -52,6 +57,7 @@ export function TheScene(props: TheSceneProps) {
         direction={new Vector3(0, -5, -10)}
         angle={Math.PI}
         exponent={20}
+        diffuse={new Color3(1, 0, 0)}
       />
       <spotLight
         name="light2"
@@ -59,6 +65,7 @@ export function TheScene(props: TheSceneProps) {
         direction={new Vector3(0, -5, 10)}
         angle={Math.PI}
         exponent={20}
+        diffuse={new Color3(0, 1, 0)}
       />
       <spotLight
         name="light3"
@@ -66,6 +73,7 @@ export function TheScene(props: TheSceneProps) {
         direction={new Vector3(10, -5, 0)}
         angle={Math.PI}
         exponent={20}
+        diffuse={new Color3(0, 0, 1)}
       />
       <spotLight
         name="light4"
@@ -73,6 +81,7 @@ export function TheScene(props: TheSceneProps) {
         direction={new Vector3(-10, -5, 0)}
         angle={Math.PI}
         exponent={20}
+        diffuse={new Color3(0, 0, 0)}
       />
       <sphere name="sphere" diameter={2} position={new Vector3(4, 1, 4)} />
       <sphere name="sphere" diameter={2} position={new Vector3(4, 1, -4)} />
